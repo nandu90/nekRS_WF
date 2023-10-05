27cc16c0d91a13ddc6b5d991b842ed915ae18d88
Nadish Saini <nsaini@anl.gov>
2023-10-05 14:05:44 -0500

    ktauSST model

--- src/plugins/RANSktau.cpp
+++ src/plugins/RANSktau.cpp
@@ -10,6 +10,7 @@ namespace {
 static nrs_t *nrs;
 
 int kFieldIndex;
+int mid;
 
 dfloat rho;
 dfloat mueLam;
@@ -19,7 +20,15 @@ static occa::memory o_mut;
 static occa::memory o_k;
 static occa::memory o_tau;
 
+static occa::memory o_xk;
+static occa::memory o_xt;
+static occa::memory o_xtq;
+static occa::memory o_OiOjSk;
+static occa::memory o_SijMag2;
+static occa::memory o_ywd;
+
 static occa::kernel computeKernel;
+static occa::kernel computeGradKernel;
 static occa::kernel mueKernel;
 static occa::kernel limitKernel;
   
@@ -42,7 +51,18 @@ static dfloat coeff[] = {
     100.0,     // fb_c2
     0.52,      // alp_inf
     1e-8,      // TINY
-    0         // Pope correction
+    0,         // Pope correction
+
+    //Additional SST parameters
+    0.85,      // sigma_k_SST
+    0.075,     // beta0_SST
+    5.0 / 9.0, // alp_inf_SST
+    0.31,      // alp1
+    0.0828,    // beta2
+    1.0,       // sigk2
+    0.856,     // sigom2
+    0.44,      // gamma2
+    1e-10      // tinySST
 };
 } // namespace
 
@@ -82,6 +102,25 @@ void RANSktau::buildKernel(occa::properties _kernelInfo)
     kernelInfo["defines/p_tiny"] = coeff[13];
   if (!kernelInfo.get<std::string>("defines/p_pope").size())
     kernelInfo["defines/p_pope"] = coeff[14];
+
+  if (!kernelInfo.get<std::string>("defines/p_sigma_k_SST").size())
+    kernelInfo["defines/p_sigma_k_SST"] = coeff[15];
+  if (!kernelInfo.get<std::string>("defines/p_beta0_SST").size())
+    kernelInfo["defines/p_beta0_SST"] = coeff[16];
+  if (!kernelInfo.get<std::string>("defines/p_alp_inf_SST").size())
+    kernelInfo["defines/p_alp_inf_SST"] = coeff[17];
+  if (!kernelInfo.get<std::string>("defines/p_alp1").size())
+    kernelInfo["defines/p_alp1"] = coeff[18];
+  if (!kernelInfo.get<std::string>("defines/p_beta2").size())
+    kernelInfo["defines/p_beta2"] = coeff[19];
+  if (!kernelInfo.get<std::string>("defines/p_sigk2").size())
+    kernelInfo["defines/p_sigk2"] = coeff[20];
+  if (!kernelInfo.get<std::string>("defines/p_sigom2").size())
+    kernelInfo["defines/p_sigom2"] = coeff[21];
+  if (!kernelInfo.get<std::string>("defines/p_gamma2").size())
+    kernelInfo["defines/p_gamma2"] = coeff[22];
+  if (!kernelInfo.get<std::string>("defines/p_tinySST").size())
+    kernelInfo["defines/p_tinySST"] = coeff[23];
   
   const int verbose = platform->options.compareArgs("VERBOSE", "TRUE") ? 1 : 0;
 
@@ -98,13 +137,9 @@ void RANSktau::buildKernel(occa::properties _kernelInfo)
   std::string fileName, kernelName;
   const std::string extension = ".okl";
   {
-    kernelName = "RANSktauComputeHex3D";
+    kernelName = "RANSktauGradHex3D";
     fileName = path + kernelName + extension;
-    computeKernel = platform->device.buildKernel(fileName, kernelInfo, true);
-    
-    kernelName = "mue";
-    fileName = path + kernelName + extension;
-    mueKernel = platform->device.buildKernel(fileName, kernelInfo, true);
+    computeGradKernel = platform->device.buildKernel(fileName, kernelInfo, true);
 
     kernelName = "limit";
     fileName = path + kernelName + extension;
@@ -113,6 +148,16 @@ void RANSktau::buildKernel(occa::properties _kernelInfo)
     kernelName = "SijMag2OiOjSk";
     fileName = path + kernelName + extension;
     SijMag2OiOjSkKernel = platform->device.buildKernel(fileName, kernelInfo, true);
+
+    const std::string headerFile = path + "RANSktauSSTBlendingFunc" + extension;
+    kernelInfo["includes"] += headerFile.c_str();
+    kernelName = "RANSktauComputeHex3D";
+    fileName = path + kernelName + extension;
+    computeKernel = platform->device.buildKernel(fileName, kernelInfo, true);
+
+    kernelName = "mue";
+    fileName = path + kernelName + extension;
+    mueKernel = platform->device.buildKernel(fileName, kernelInfo, true);
   }
 
   int Nscalar;
@@ -132,7 +177,23 @@ void RANSktau::updateProperties()
   occa::memory o_diff = cds->o_diff + cds->fieldOffsetScan[kFieldIndex];
 
   limitKernel(mesh->Nelements * mesh->Np, o_k, o_tau);
-  mueKernel(mesh->Nelements * mesh->Np, nrs->fieldOffset, rho, mueLam, o_k, o_tau, o_mut, o_mue, o_diff);
+
+  occa::memory o_SijOij = platform->o_memPool.reserve<dfloat>(3 * nrs->NVfields * nrs->fieldOffset);
+  postProcessing::strainRotationRate(nrs, true, true, o_SijOij);
+
+  SijMag2OiOjSkKernel(mesh->Nelements * mesh->Np, nrs->fieldOffset, 1, o_SijOij, o_OiOjSk, o_SijMag2);
+
+  computeGradKernel(mesh->Nelements,
+                    nrs->cds->fieldOffset[kFieldIndex],
+                    mesh->o_vgeo,
+                    mesh->o_D,
+                    o_k,
+                    o_tau,
+                    o_xk,
+                    o_xt,
+                    o_xtq);
+
+  mueKernel(mesh->Nelements * mesh->Np, nrs->fieldOffset, mid, rho, mueLam, o_k, o_tau, o_SijMag2, o_xk, o_ywd, o_mut, o_mue, o_diff);
 }
 
 occa::memory RANSktau::o_mue_t() { return o_mut; }
@@ -142,35 +203,40 @@ void RANSktau::updateSourceTerms()
   mesh_t *mesh = nrs->meshV;
   cds_t *cds = nrs->cds;
 
-  occa::memory o_OiOjSk = platform->o_memPool.reserve<dfloat>(nrs->fieldOffset);
-  occa::memory o_SijMag2 = platform->o_memPool.reserve<dfloat>(nrs->fieldOffset); 
-  occa::memory o_SijOij = platform->o_memPool.reserve<dfloat>(3 * nrs->NVfields * nrs->fieldOffset); 
-
   occa::memory o_FS = cds->o_FS + cds->fieldOffsetScan[kFieldIndex];
   occa::memory o_BFDiag = cds->o_BFDiag + cds->fieldOffsetScan[kFieldIndex];
-
-  postProcessing::strainRotationRate(nrs, true, true, o_SijOij);
-
-  SijMag2OiOjSkKernel(mesh->Nelements * mesh->Np, nrs->fieldOffset, 1, o_SijOij, o_OiOjSk, o_SijMag2);
     
-  computeKernel(mesh->Nelements,
+  computeKernel(mesh->Nelements * mesh->Np,
                 nrs->cds->fieldOffset[kFieldIndex],
+                mid,
                 rho,
                 mueLam,
-		mesh->o_vgeo,
-                mesh->o_D,
                 o_k,
                 o_tau,
                 o_SijMag2,
                 o_OiOjSk,
+                o_xk,
+                o_xt,
+                o_xtq,
+                o_ywd,
 		o_BFDiag,
                 o_FS);
 }
 
-void RANSktau::setup(nrs_t *nrsIn, dfloat mueIn, dfloat rhoIn, int ifld)
+void RANSktau::setup(nrs_t *nrsIn, dfloat mueIn, dfloat rhoIn, int ifld, std::string & model)
 {
   if (setupCalled)
     return;
+
+  upperCase(model);
+  if(model.compare("DEFAULT") == 0 || model.compare("KTAU") == 0) mid = 0;
+  if(model.compare("SST") == 0){
+    mid = 1;
+    if(o_ywd == o_NULL){
+      printf("\nSST model requires wall distance\nCheck usage\n");
+      exit(1);
+    }
+  }
 
   nrs = nrsIn;
   mueLam = mueIn;
@@ -190,5 +256,19 @@ void RANSktau::setup(nrs_t *nrsIn, dfloat mueIn, dfloat rhoIn, int ifld)
     platform->linAlg->fill(cds->fieldOffsetSum, 0.0, cds->o_BFDiag);
   }
 
+  o_OiOjSk = platform->device.malloc<dfloat>(nrs->fieldOffset);
+  o_SijMag2 = platform->device.malloc<dfloat>(nrs->fieldOffset);
+  o_xk = platform->device.malloc<dfloat>(cds->fieldOffset[kFieldIndex]);
+  o_xt = platform->device.malloc<dfloat>(cds->fieldOffset[kFieldIndex]);
+  o_xtq = platform->device.malloc<dfloat>(cds->fieldOffset[kFieldIndex]);
+
   setupCalled = 1;
+}
+
+void RANSktau::setup(nrs_t *nrsIn, dfloat mueIn, dfloat rhoIn, int ifld, std::string & model, double *ywd)
+{
+  o_ywd = platform->device.malloc<dfloat>(nrsIn->fieldOffset);
+  o_ywd.copyFrom(ywd, nrsIn->fieldOffset);
+
+  RANSktau::setup(nrsIn, mueIn, rhoIn, ifld, model);
 }
